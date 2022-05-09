ab2d7aa7ce2f7bfd9d992c5c5105dff49e0901d9
Will Gundy <willgunderson503@gmail.com>
2022-05-09 13:44:11 -0700

    added beanie detail page

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ export default function App() {
     <Router>
       <div className='App'>
         <Switch>
-          <Route path='/'>
-            {/* this home page route should list all the beanies */}
-            <BeaniesPage />
-          </Route>
           <Route path='/beanie/:id'> 
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
             <BeanieDetail />
+          </Route>
+          <Route path='/'>
+            {/* this home page route should list all the beanies */}
+            <BeaniesPage />
           </Route>
         </Switch>
       </div>
--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -10,8 +10,15 @@ export default function BeanieDetail() {
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
+    async function loadBeanie() {
+      const beanieData = await getSingleBeanie(params.id);
 
-  }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
+      setBeanieBaby(beanieData);
+    }
+
+    loadBeanie();
+
+  }, [params.id]); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
 
   function handleBeanieClick() {
     // here's a challenge. How can you link on click to the beanie baby's correct entry in the official beanie baby fan site?
@@ -21,7 +28,7 @@ export default function BeanieDetail() {
     <>
       {/* Feel free to uncomment and use the below code--but you'll need to figure out how to keep it from blowing up on load */}
       
-      {/* <Link to='/'>Home</Link>
+      <Link to='/'>Home</Link>
       <div className='beanie-detail' onClick={handleBeanieClick}>
         <div className='beanie-data'>
           <p>{beanieBaby.animal}</p>
@@ -40,7 +47,7 @@ export default function BeanieDetail() {
           <p>Swing Tag Generation: {beanieBaby.swingTagGeneration}</p>
           <p>Tush Tag Generation: {beanieBaby.tushTagGeneration}</p>
         </div>
-      </div>   */}
+      </div>  
     </>
   );
 }
